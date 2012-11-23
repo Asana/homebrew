78f3ebe52873e96e5d7acd66b56188558796c174
S. Alex Smith <alex@asana.com>
2012-11-22 17:29:03 -0800

    Updates thrift to use libevent.
    This causes ThriftNB to be built.

--- Library/Formula/thrift.rb
+++ Library/Formula/thrift.rb
@@ -2,6 +2,8 @@ require 'formula'
 
 class Thrift < Formula
   homepage 'http://thrift.apache.org'
+  # Asana note: This doesn't agree with the version of thrift that we get for fb303.
+  # This should be fine.
   url 'http://www.apache.org/dyn/closer.cgi?path=thrift/0.8.0/thrift-0.8.0.tar.gz'
   sha1 '1d652d7078d9cc70e2a45d3119b13e86ebd446da'
 
@@ -14,6 +16,7 @@ class Thrift < Formula
   option "with-php", "Install Php binding"
 
   depends_on 'boost'
+  depends_on 'libevent'
 
   def install
     # No reason for this step is known. On Lion at least the pkg.m4 doesn't
@@ -37,6 +40,7 @@ class Thrift < Formula
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}",
                           "--libdir=#{lib}",
+                          "--with-libevent=#{HOMEBREW_PREFIX}",
                           *exclusions
     ENV.j1
     system "make"
