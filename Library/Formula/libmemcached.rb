f1ac21e11bf7216ea53598e6bb05aeb412bcc346
S. Alex Smith <alex@asana.com>
2012-11-26 00:19:22 -0800

    Downgrade libmemcached version.
    As the comment says, somewhere between here and version 1.0.8,
    v8cgi starts segfaulting.

--- Library/Formula/libmemcached.rb
+++ Library/Formula/libmemcached.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class Libmemcached < Formula
   homepage 'http://libmemcached.org'
-  url 'http://launchpad.net/libmemcached/1.0/1.0.8/+download/libmemcached-1.0.8.tar.gz'
-  sha1 'ac1925aea002d8ad28d709aa374d057c73e322af'
+  # Asana: We use a quite old version of libmemcached. Somewhere between here and 1.0.8, v8cgi starts getting segfaults.
+  url 'https://launchpad.net/libmemcached/1.0/0.40/+download/libmemcached-0.40.tar.gz'
+  sha1 'cba48c9816e19ecc0b0c71907c122286fd929780'
 
   depends_on 'memcached'
 
